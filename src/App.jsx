d8c02b02a15e75cb453a1a25d111d99d94e930a0
tris1412 <tdas_cse2305j6@mgit.ac.in>
2026-08-19 21:00:59 +0530

    Test test environment deployment

--- src/App.jsx
+++ src/App.jsx
@@ -703,7 +703,7 @@ function App() {
       <main className="main-content">
         <header className="topbar">
           <div>
-            <p className="greeting">Good afternoon 👋</p>
+            <p className="greeting">Good afternoon 👋 -TEST </p>
 
             <h2>{activePage}</h2>
           </div>
